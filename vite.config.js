7548e31fdb6db85baa6707a34c3ba6085a9e00d3
Yosei Ushida <ushida.yosei@gmail.com>
2024-11-30 21:37:38 +0900

    PWA対応

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'; 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: '文学フリマ東京39 ブース検索',
+        short_name: '文フリブース検索',
+        description: '文学フリマ東京39のブース検索アプリ',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => url.origin === self.origin,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'local-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30日間
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
 })
